bc40d8fcd6de957839ceb5c1d0d5d1cdae56617d
konglobemeralt <jesper.blidkvist@live.se>
2017-04-18 17:45:44 +0200

    fixed rotation classes and added documentation

--- Assets/Standard Assets/Scripts/buttonRotateX.cs
+++ Assets/Standard Assets/Scripts/buttonRotateX.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:221174af6c105d96bd4790772cb02a0eee59c37ea6251643c8e4fe47bbd86959
-size 669
+oid sha256:6c6ce0ee792eac5597251c8de22bb538b04b0f8981740d904b970ceb1b7cfab7
+size 743
--- Assets/Standard Assets/Scripts/buttonRotateY.cs
+++ Assets/Standard Assets/Scripts/buttonRotateY.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b87d8b6bb86d47e2f50745f46b2907cf955d4fbfbd0a1d76632c796b6aed1f7c
-size 664
+oid sha256:9668c2d84b79b73065a31a9490f4a9a63ebb9db0f09463c06c52480d85b12226
+size 738
