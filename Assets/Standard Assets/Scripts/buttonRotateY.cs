aad00d2dc46a955de46cb29aa889ae54333aae61
konglobemeralt <jesper.blidkvist@live.se>
2017-04-18 17:49:45 +0200

    Added Documentation for button translation

--- Assets/Standard Assets/Scripts/buttonRotateY.cs
+++ Assets/Standard Assets/Scripts/buttonRotateY.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9668c2d84b79b73065a31a9490f4a9a63ebb9db0f09463c06c52480d85b12226
-size 738
+oid sha256:308288dcd28868ce658ec21943fafc0942fea22bd168cd1577b2f65363abcdef
+size 791
--- Assets/Standard Assets/Scripts/buttonStrafe.cs
+++ Assets/Standard Assets/Scripts/buttonStrafe.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff57559e97d5db6b0b4098dc57f8f52de131c59a3d846c2e733a42edf3f9be46
-size 656
+oid sha256:6a404d4ab89f272e93b81c31d7c5075c980dc081a3ebef19c049e7cf95ff7639
+size 793
