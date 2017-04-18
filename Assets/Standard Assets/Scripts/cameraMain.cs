d034c47b1b8134886301d83381c3858ef2eb6982
konglobemeralt <jesper.blidkvist@live.se>
2017-04-18 18:49:27 +0200

    Slight fixes in cameraMain

--- Assets/Standard Assets/Scripts/cameraMain.cs
+++ Assets/Standard Assets/Scripts/cameraMain.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bc0c12a1b19dc45e39f3ae7a98b5f9262cc6d9f54421124aea9b7ff37906325
-size 12200
+oid sha256:e6db78f26311300608b7ea5a20adee5f390bb778185e0b9e64bdcc44396a284e
+size 12230
