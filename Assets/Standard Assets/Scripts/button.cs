07ca98b4a9064318de9223e51933969b5d1b7b9a
konglobemeralt <jesper.blidkvist@live.se>
2017-04-18 17:31:54 +0200

    Removed old Lightmap, started documentation

--- Assets/Standard Assets/Scripts/button.cs
+++ Assets/Standard Assets/Scripts/button.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:638a58fcd3cbe61be29df8bfc366a4dd6370a3fe18628fc2b4a07bff502aa94a
-size 659
+oid sha256:6a5772d7b8f3f790622dd9d0c94f86e4e9595e367a2908814858ad321d8a5ff2
+size 775
