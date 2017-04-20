03c54153d42fe4f847de0753ac520fc4f7eef165
konglobemeralt <jesper.blidkvist@live.se>
2017-04-20 14:25:51 +0200

    Improved cameraMain.cs

--- Assets/Standard Assets/Scripts/cameraMain.cs
+++ Assets/Standard Assets/Scripts/cameraMain.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6af651e63b1c0452fe5ce9c33e32b30fb3d2b57b2ef30b7205850791a352f63b
-size 12217
+oid sha256:49b2fa3041b440963d9fbb1015caa6d4e304ccca50c0a0ef1f0ada9677115bef
+size 11911
