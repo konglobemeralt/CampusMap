1c0b820a4bb61e266c22bfc702795322134be024
konglobemeralt <jesper.blidkvist@live.se>
2017-04-20 13:12:25 +0200

    Fixed mouseMain scritp

--- Assets/Standard Assets/Scripts/mouseMain.cs
+++ Assets/Standard Assets/Scripts/mouseMain.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec433bf39573f2668b92af605e413ab4d7f18edc8000310795f41e81c38157cb
-size 1987
+oid sha256:5d14d68584de092b7a5dd249c233b1d626f35198cfe05f260f2c8ef6c3954772
+size 2452
