1a849f9fd7471ff137069e41852a30ab778d3a3b
konglobemeralt <jesper.blidkvist@live.se>
2017-04-20 14:34:24 +0200

    Fixing the lerp

--- Assets/Standard Assets/Scripts/cameraMain.cs
+++ Assets/Standard Assets/Scripts/cameraMain.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49b2fa3041b440963d9fbb1015caa6d4e304ccca50c0a0ef1f0ada9677115bef
-size 11911
+oid sha256:e620afc972026ad54d5f34ea9f49c5ea085dc8bd6f942bab778edd42c6d5b1b2
+size 11828
