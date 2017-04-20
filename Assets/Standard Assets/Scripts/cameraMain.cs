32277e3fed5063d453936064b7f5a419d13d17d2
konglobemeralt <jesper.blidkvist@live.se>
2017-04-20 13:11:53 +0200

    Fixed marker scritp

--- Assets/Standard Assets/Scripts/cameraMain.cs
+++ Assets/Standard Assets/Scripts/cameraMain.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2910cf8d34a618ff2f10d51af68102e5475b071116af76ab2428dca9a7be751c
-size 12181
+oid sha256:6af651e63b1c0452fe5ce9c33e32b30fb3d2b57b2ef30b7205850791a352f63b
+size 12217
