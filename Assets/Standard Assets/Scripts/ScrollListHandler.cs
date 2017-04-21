7173294400ea06df729f5befd14be2e76d1db28f
konglobemeralt <jesper.blidkvist@live.se>
2017-04-21 19:59:34 +0200

    More refactoring in scrollListHandler

--- Assets/Standard Assets/Scripts/ScrollListHandler.cs
+++ Assets/Standard Assets/Scripts/ScrollListHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adec3d9092c108cd193cdfb667c02860697da61958a369c74dce4f4fa651db3c
-size 3766
+oid sha256:2992718dee3f00bbcfe6b47847c2ec5d7f7201d56de5c4793ff6c21d026a2139
+size 3795
