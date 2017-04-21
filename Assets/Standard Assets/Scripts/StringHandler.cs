4f7902726704e78b6e2ea90f71ae7adb883a3c7e
konglobemeralt <jesper.blidkvist@live.se>
2017-04-21 15:31:22 +0200

    added popUpHandler for help window and similiar windows

--- Assets/Standard Assets/Scripts/StringHandler.cs
+++ Assets/Standard Assets/Scripts/StringHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0cb5a68c48523c34efaf4e5786032c9f90b788bcd69b2c1ffbfc0f066dbf3a7
-size 7249
+oid sha256:dbaa31f333b5204d00eb7aa96f6dfa7661f00adec3fb3dc90cfeb6a59f68e0b2
+size 6439
